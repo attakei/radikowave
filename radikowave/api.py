2e18e2de910bb756dbb93c48bc50560c4026cef3
attakei <attakei@gmail.com>
2015-09-06 23:48:57 +0900

    Mod: RadikoArea has all prefectures code, and RadikoApi has area instead of area_id.
    - Area id is based from http://jprs.jp/about/jp-dom/prefecture.html#labels
    - RadikoArea that return area-id for Radiko-API. If Area is Hokkaido, area_id return 'JP1'.

--- radikowave/api.py
+++ radikowave/api.py
@@ -4,9 +4,69 @@ from enum import Enum
 __author__ = 'attakei'
 
 
-class RadikoArea(Enum):
+class _RadikoArea(Enum):
+    """Behavior extension for RadikoArea.
+    """
+    def get_id(self):
+        return 'JP{}'.format(self.value)
+
+    @property
+    def area_id(self):
+        return self.get_id()
+
+
+class RadikoArea(_RadikoArea):
+    """Radiko Area id enumerations.
+
+    Names is based from http://jprs.jp/about/jp-dom/prefecture.html#labels
+    """
+    Hokkaido = 1
+    Aomori = 2
+    Iwate = 3
+    Miyagi = 4
+    Akita = 5
+    Yamagata = 6
+    Fukushima = 7
+    Ibaraki = 8
+    Tochigi = 9
+    Gunma = 10
+    Saitama = 11
     Chiba = 12
     Tokyo = 13
+    Kanagawa = 14
+    Niigata = 15
+    Yamanashi = 16
+    Nagano = 17
+    Ishikawa = 18
+    Toyama = 19
+    Fukui = 20
+    Aichi = 21
+    Gifu = 22
+    Shizuoka = 23
+    Mie = 24
+    Osaka = 25
+    Hyogo = 26
+    Kyoto = 27
+    Shiga = 28
+    Nara = 29
+    Wakayama = 30
+    Okayama = 31
+    Hiroshima = 32
+    Tottori = 33
+    Shimane = 34
+    Yamaguchi = 35
+    Kagawa = 36
+    Tokushima = 37
+    Ehime = 38
+    Kochi = 39
+    Fukuoka = 40
+    Saga = 41
+    Nagasaki = 42
+    Kumamoto = 43
+    Oita = 44
+    Miyazaki = 45
+    Kagoshima = 46
+    Okinawa = 47
 
 
 class RadikoApi(object):
@@ -15,5 +75,5 @@ class RadikoApi(object):
     ENDPOINT = 'http://radiko.jp/v2'
     DEFAULT_AREA = RadikoArea.Tokyo
 
-    def __init__(self, area_id=None):
-        self.area_id = area_id if area_id is not None else self.DEFAULT_AREA
+    def __init__(self, area=None):
+        self.area = area if area is not None else self.DEFAULT_AREA
--- tests/test_api.py
+++ tests/test_api.py
@@ -5,11 +5,20 @@ from radikowave.api import RadikoApi, RadikoArea
 __author__ = 'attakei'
 
 
+class AreaTest(unittest.TestCase):
+    def test_get_id(self):
+        self.assertEqual(RadikoArea.Hokkaido.get_id(), 'JP1')
+        self.assertEqual(RadikoArea.Okinawa.get_id(), 'JP47')
+
+    def test_as_property(self):
+        self.assertEqual(RadikoArea.Hokkaido.area_id, 'JP1')
+
+
 class ApiTest(unittest.TestCase):
     def test_init_default_area(self):
         api = RadikoApi()
-        self.assertEqual(api.area_id, RadikoArea.Tokyo)
+        self.assertEqual(api.area, RadikoArea.Tokyo)
 
     def test_init_specify_area(self):
         api = RadikoApi(RadikoArea.Chiba)
-        self.assertEqual(api.area_id, RadikoArea.Chiba)
+        self.assertEqual(api.area, RadikoArea.Chiba)
